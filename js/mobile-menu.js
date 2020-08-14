210feae2e30954cfeb2f253aa1f38de8f10fff74
pngur <apangur@yandex.ru>
2020-08-14 03:28:25 +0300

    Добавил анимацию скроллианга. Фавиконки. Поправил дефект в мобильной вёрстки

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,13 @@
 const burgerButton = document.querySelector('.mobile-menu');
 const burgerClass = document.querySelector('.mobile-menu__burger-icon');
 const mobileMenu = document.querySelector('.navigation');
+const linkList = document.querySelector('.navigation__list');
+const linkItems = document.querySelectorAll('.navigation__link');
+const bodyTag = document.body;
 
-// burgerButton.addEventListener('click', function() {
-// });
-
-burgerButton.addEventListener('click', function() {
+burgerButton.addEventListener('click', function(e) {
+   
    mobileMenu.classList.toggle('toggle-js');
-
    if (burgerClass.classList.contains('mobile-menu__burger-icon')) {
       burgerClass.classList.remove('mobile-menu__burger-icon'); 
       burgerClass.classList.add('mobile-menu__cross-icon'); 
@@ -15,4 +15,56 @@ burgerButton.addEventListener('click', function() {
       burgerClass.classList.remove('mobile-menu__cross-icon'); 
       burgerClass.classList.add('mobile-menu__burger-icon'); 
    }
+   
 });
+
+linkList.addEventListener('click', function(e) {
+  
+   for (let i of linkItems) {
+      if (i === e.target) {
+         mobileMenu.classList.toggle('toggle-js')};
+         burgerClass.classList.remove('mobile-menu__cross-icon'); 
+         burgerClass.classList.add('mobile-menu__burger-icon'); 
+   }
+});
+
+
+///Smooth Scrolling
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            // $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
